9e2fb4523bde1184075e4fb8e5fcec66a352f460
eche10n <46045580+eche10n@users.noreply.github.com>
2019-01-04 13:09:39 +0300

    Add files via upload

---
+++ entity.h
@@ -0,0 +1,42 @@
+#include <SFML/Graphics.hpp>
+#include "animation.h"
+#pragma once
+
+using namespace sf;
+
+class Entity
+{
+public: // класс сущности
+	float x, y, dx, dy, R, angle;
+	bool life;
+	std::string name;
+	Animation anim;
+
+	Entity()
+	{
+		life = 1; // жизнь сущности (0, если объект уничтожается)
+	}
+
+	void settings(Animation &a, int X, int Y, float Angle = 0, int radius = 1) // задание настроек сущности
+	{
+		anim = a;
+		x = X; y = Y;
+		angle = Angle;
+		R = radius;
+	}
+
+	virtual void update() {};
+
+	void draw(RenderWindow &app) {
+		anim.sprite.setPosition(x, y);
+		anim.sprite.setRotation(angle + 90);
+		app.draw(anim.sprite);
+
+		CircleShape circle(R);
+		circle.setFillColor(Color(255, 0, 0, 170));
+		circle.setPosition(x, y);
+		circle.setOrigin(R, R);
+	}
+
+	virtual ~Entity() {}; // деструктор
+};
